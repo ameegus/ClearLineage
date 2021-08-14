2028687f78a51ad31bf003d77871dc3a9f059a14
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-08-14 20:20:29 +0200

    added blissrom xtended clear values

---
+++ install-to-device.sh
@@ -0,0 +1,6 @@
+adb root
+adb shell su -c mount -o rw,remount /vendor
+adb push android/build/outputs/apk/debug/android-debug.apk /vendor/overlay/ClearLineage-android.apk
+adb push systemui/build/outputs/apk/debug/systemui-debug.apk /vendor/overlay/ClearLineage-systemui.apk
+sleep 5
+adb reboot
