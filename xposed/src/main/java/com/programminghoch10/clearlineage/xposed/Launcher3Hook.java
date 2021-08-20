b811041fe7756fca618ea52e75538ac50046127d
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-08-21 00:14:50 +0200

    add launcher app overview transparency

---
+++ xposed/src/main/java/com/programminghoch10/clearlineage/xposed/Launcher3Hook.java
@@ -0,0 +1,35 @@
+package com.programminghoch10.clearlineage.xposed;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.util.AttributeSet;
+
+import java.lang.reflect.Field;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Launcher3Hook {
+	public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+		XposedBridge.log("Hooking Launcher3");
+		
+		Class<?> scrimviewclass = XposedHelpers.findClass("com.android.launcher3.views.ScrimView", lpparam.classLoader);
+		XposedHelpers.findAndHookConstructor(scrimviewclass, Context.class, AttributeSet.class, new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				Field mEndScrim = XposedHelpers.findField(scrimviewclass, "mEndScrim");
+				//int prevcolor = mEndScrim.getInt(param.thisObject);
+				/*int prevcolor = ((Context)param.args[0]).getTheme().obtainStyledAttributes(new int[] {
+						android.R.attr.colorBackground
+				}).getColor(0, 0);*/
+				boolean night = (((Context)param.args[0]).getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+				int prevcolor = night ? Color.BLACK : Color.WHITE;
+				int newcolor = (prevcolor & 0x00FFFFFF) | (0xa0 << 24);
+				mEndScrim.setInt(param.thisObject, newcolor);
+			}
+		});
+	}
+}
