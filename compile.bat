3f6610285471a8c3d74707c6bc2bdb5ddaadf076
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-08-22 14:48:11 +0200

    move xposed module

--- compile.bat
+++ compile.bat
@@ -1,9 +1,10 @@
 call gradlew.bat assembleDebug
+rmdir /S /Q magiskmodule\system
 mkdir magiskmodule\system\product\overlay
-mkdir magiskmodule\system\priv-app\clearlineage
+mkdir magiskmodule\system\app\clearlineage
 copy android\build\outputs\apk\debug\android-debug.apk magiskmodule\system\product\overlay
 copy systemui\build\outputs\apk\debug\systemui-debug.apk magiskmodule\system\product\overlay
-copy xposed\build\outputs\apk\debug\xposed-debug.apk magiskmodule\system\priv-app\clearlineage\clearlineage.apk
+copy xposed\build\outputs\apk\debug\xposed-debug.apk magiskmodule\system\app\clearlineage\clearlineage.apk
 cd magiskmodule
 tar -a -c -f ..\ClearLineage.zip *
 cd ..
