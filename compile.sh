bb7803fd897c86a4c86bf79db79a263fca605748
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-11-09 15:25:58 +0100

    reduce compile verbosity

--- compile.sh
+++ compile.sh
@@ -4,13 +4,13 @@ rm -v ClearLineage.zip
 rm -r magiskmodule/system
 rm -r magiskmodule/files
 for SDK in 30 31; do
-    mkdir -v -p magiskmodule/files/sdk$SDK/system/product/overlay
+    mkdir -p magiskmodule/files/sdk$SDK/system/product/overlay
     for VERSION in android systemui; do
         cp -v $VERSION-sdk$SDK/build/outputs/apk/release/$VERSION-sdk$SDK-release.apk magiskmodule/files/sdk$SDK/system/product/overlay/ClearLineage-$VERSION-sdk$SDK.apk
     done
 done
-mkdir -v -p magiskmodule/system/app/clearlineage
+mkdir -p magiskmodule/system/app/clearlineage
 cp -v xposed/build/outputs/apk/release/xposed-release.apk magiskmodule/system/app/clearlineage/ClearLineage-xposed.apk
 cd magiskmodule
-zip -v -r -0 ../ClearLineage.zip *
+zip -q -r -0 ../ClearLineage.zip *
 cd ..
