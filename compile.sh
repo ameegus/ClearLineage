9aff8e3804d9d579adf716dd469c1f9f141ea3ed
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-11-11 13:43:15 +0100

    add proper version codes to magisk module

--- compile.sh
+++ compile.sh
@@ -11,6 +11,21 @@ for SDK in 30 31; do
 done
 mkdir -p magiskmodule/system/app/clearlineage
 cp -v xposed/build/outputs/apk/release/xposed-release.apk magiskmodule/system/app/clearlineage/ClearLineage-xposed.apk
+VERSION=$(git log -1 --pretty=%h)
+#VERSION=$(date +%Y%m%d-%H%M%S)
+VERSIONCODE=$(git rev-list --count HEAD)
+ID=$(cat module.prop | grep "id" | cut -d "=" -f2)
+NAME=$(cat module.prop | grep "name" | cut -d "=" -f2)
+AUTHOR=$(cat module.prop | grep "author" | cut -d "=" -f2)
+DESC=$(cat module.prop | grep "description" | cut -d "=" -f2)
+echo "id=$ID
+name=$NAME
+version=$VERSION
+versionCode=$VERSIONCODE
+author=$AUTHOR
+description=$DESC" > magiskmodule/module.prop
 cd magiskmodule
+#sed -i "s/version=.*/version=$VERSION/g" module.prop
+#sed -i "s/versionCode=.*/versionCode=$VERSIONCODE/g" module.prop
 zip -q -r -0 ../ClearLineage.zip *
 cd ..
