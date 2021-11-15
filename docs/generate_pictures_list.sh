9e00aa3789bb3c3727fecb135b71d3c3d0b64749
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-11-15 15:58:14 +0100

    add pictures

--- docs/generate_pictures_list.sh
+++ docs/generate_pictures_list.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cd images
 echo "[" > pictures.json
 for PICTURE in $(ls *.png *.jpg); do
   echo "  \"$PICTURE\"," >> pictures.json
 done
 echo "]" >> pictures.json
+cd ..
+mv -f images/pictures.json pictures.json
 sed -i 'x;${s/,$//;p;x;};1d' pictures.json
