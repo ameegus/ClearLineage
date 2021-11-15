0c0f7db10937076b1e22ff7e116f3f32aac1f80d
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-11-15 16:12:35 +0100

    shuffle images

--- docs/script.js
+++ docs/script.js
@@ -13,6 +13,22 @@ Array.prototype.getSafe = function (index) {
   return this[index];
 };
 
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ * from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ */
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
 let motds = [
   "Transparency for LineageOS",
   "Transparency since R",
@@ -95,6 +111,7 @@ function updatePictures() {
   if (pictureSetBefore == null) {
     pictureSetBefore = pictures;
   }
+  pictures = shuffle(pictures)
 
   renderHandlebars(getel("images"), {
     pictures: pictureSetBefore.map((e) => ({
