fc1568babbd2b1a750231249cd190c716c75028b
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-12-06 16:51:35 +0100

    update module install script

--- magiskmodule/customize.sh
+++ magiskmodule/customize.sh
@@ -6,22 +6,20 @@ case $API in
 	30)
 		;;
 	31)
-		ui_print "Android 12 support is experimental!"
 		;;
 	*)
 		abort "Your Android version is not supported!"
 		;;
 esac
 
-ui_print
-ui_print "Welcome to the ClearLineage Revolution"
-ui_print
-
-ui_print "- Copying files..."
 for FOLDER in $(ls "$MODPATH/files/sdk$API"); do
-	cp -rv "$MODPATH/files/sdk$API/$FOLDER" "$MODPATH"
+	cp -r "$MODPATH/files/sdk$API/$FOLDER" "$MODPATH"
 done
 for FOLDER in $(ls "$MODPATH/files/all"); do
-	cp -rv "$MODPATH/files/all/$FOLDER" "$MODPATH"
+	cp -r "$MODPATH/files/all/$FOLDER" "$MODPATH"
 done
 rm -r "$MODPATH/files"
+
+ui_print
+ui_print "Welcome to the ClearLineage Revolution!"
+ui_print
