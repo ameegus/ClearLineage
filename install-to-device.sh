6a06c2786f3a2d6b7dbbd93f57b66532d4e098d3
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-12-20 19:36:54 +0100

    update install script

--- install-to-device.sh
+++ install-to-device.sh
@@ -1,12 +1,37 @@
+#!/bin/bash
+
 FOLDER=/sdcard
 SUFOLDER=/data/adb
-adb shell touch $FOLDER/ClearLineage.zip
-if [ $? -eq 0 ]; then
+
+pushasuser() {
+    echo "Pushing as user to $FOLDER"
     adb push ClearLineage.zip $FOLDER
     adb shell su -c magisk --install-module $FOLDER/ClearLineage.zip
-else
+    exit $?
+}
+
+pushasroot() {
     adb root || exit 1
+    echo "Pushing as root to $SUFOLDER"
     adb push ClearLineage.zip $SUFOLDER
     adb shell magisk --install-module $SUFOLDER/ClearLineage.zip
+    exit_code=$?
     adb shell rm -v $SUFOLDER/ClearLineage.zip
+    exit $exit_code
+}
+
+# manual selection passed as parameter
+if [[ "$1" == "root" ]]; then
+    pushasroot
+fi
+if [[ "$1" == "user" ]]; then
+    pushasroot
+fi
+
+# detect automatically
+adb shell touch $FOLDER/ClearLineage.zip
+if [ $? -eq 0 ]; then
+    pushasuser
+else
+    pushasroot
 fi
