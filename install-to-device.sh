27770759a4618ca8db5996e11c9c9d680c73c707
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-12-30 19:03:29 +0100

    update install script

--- install-to-device.sh
+++ install-to-device.sh
@@ -3,35 +3,97 @@
 FOLDER=/sdcard
 SUFOLDER=/data/adb
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 [push mode: auto/user/root] [reboot yes/no] [adb device identifier]"
+    echo "Defaults:"
+    echo "  Push mode: auto"
+    echo "  Reboot after sucessful install: no"
+    echo "  The install is performed on all available adb devices."
+    exit 0
+fi
+
+PUSHTYPE="$1"
+REBOOT="$2"
+DEVICE="$3"
+
+if [ -z "$REBOOT" ]; then REBOOT="no"; fi
+if [ -z "$PUSHTYPE" ]; then PUSHTYPE="auto"; fi
+if [ "$PUSHTYPE" = "auto" ]; then PUSHTYPE="preferroot"; fi
+
+if [ -z $DEVICE ]; then
+    DEVICES="$(adb devices -l | grep transport_id | cut -d' ' -f 1)"
+    DEVICESCOUNT="$(adb devices -l | grep transport_id | cut -d' ' -f 1 | wc -l)"
+    if [ "$DEVICESCOUNT" = "0" ]; then 
+        echo "No devices found!"
+        exit 1
+    fi
+    if [ $DEVICESCOUNT -gt 1 ]; then
+        echo "Found $DEVICESCOUNT devices"
+        for device in $DEVICES; do
+            echo "installing for $device"
+            $0 $PUSHTYPE $REBOOT $device
+        done
+        exit 0
+    fi
+fi
+
+ADB="adb"
+if [ ! -z "$DEVICE" ]; then ADB="$ADB -s $DEVICE"; fi
+
+rebootifwanted() {
+    prev_exit_code="$1"
+    if [ -z "$prev_exit_code" ]; then $prev_exit_code=$?; fi
+    if [ "$REBOOT" != "yes" ]; then return; fi
+    if [ "$prev_exit_code" = "0" ]; then $ADB reboot; fi
+}
+
 pushasuser() {
     echo "Pushing as user to $FOLDER"
-    adb push ClearLineage.zip $FOLDER
-    adb shell su -c magisk --install-module $FOLDER/ClearLineage.zip
-    exit $?
+    $ADB push ClearLineage.zip $FOLDER
+    $ADB shell su -c magisk --install-module $FOLDER/ClearLineage.zip
+    exit_code="$?"
+    rebootifwanted "$exit_code"
+    exit $exit_code
+}
+
+waitfordevice() {
+    if [ -z "$DEVICE" ]; then adb wait-for-device; return; fi
+    while [ "$(adb devices -l | grep $DEVICE | wc -l)" = "0" ]; do
+        sleep 1
+    done
 }
 
 pushasroot() {
-    adb root || exit 1
+    $ADB root
+    waitfordevice
+    if [ "$($ADB shell id -u)" != "0" ]; then pushasuser; fi
     echo "Pushing as root to $SUFOLDER"
-    adb push ClearLineage.zip $SUFOLDER
-    adb shell magisk --install-module $SUFOLDER/ClearLineage.zip
+    $ADB push ClearLineage.zip $SUFOLDER
+    $ADB shell magisk --install-module $SUFOLDER/ClearLineage.zip
     exit_code=$?
-    adb shell rm -v $SUFOLDER/ClearLineage.zip
+    $ADB shell rm -v $SUFOLDER/ClearLineage.zip
+    rebootifwanted "$exit_code"
     exit $exit_code
 }
 
 # manual selection passed as parameter
-if [[ "$1" == "root" ]]; then
+if [ "$PUSHTYPE" = "root" ]; then
     pushasroot
 fi
-if [[ "$1" == "user" ]]; then
+if [ "$PUSHTYPE" = "user" ]; then
     pushasroot
 fi
 
 # detect automatically
-adb shell touch $FOLDER/ClearLineage.zip
-if [ $? -eq 0 ]; then
-    pushasuser
-else
+if [ "$PUSHTYPE" = "preferuser" ]; then
+    $ADB shell touch $FOLDER/ClearLineage.zip
+    if [ $? -eq 0 ]; then
+        pushasuser
+    else
+        pushasroot
+    fi
+fi
+if [ "$PUSHTYPE" = "preferroot" ]; then
+    # just push as root, if it fails it will automatically try pushing as user
     pushasroot
 fi
